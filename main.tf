a54a09e790727b5ada2d19a26642672d3338a015
ashashurin <ashashurin@stsoft.ru>
2022-01-13 15:07:22 +0300

    aws_cred

--- main.tf
+++ main.tf
@@ -1,7 +1,8 @@
 provider "aws" {
   region = var.region
+  access_key = var.aws_access_key_id
+  secret_key = var.aws_secret_access_key
 }
-
 
 resource "aws_instance" "wg" {
   ami                    = data.aws_ami.ubuntu20_04.id
